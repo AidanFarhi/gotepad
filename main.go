02074064b9c0312122ef97cebcf20de3141c8831
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-10-20 00:08:48 -0400

    added some example code

--- main.go
+++ main.go
@@ -1,8 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"os"
+	"path/filepath"
+
+	_ "modernc.org/sqlite"
+)
 
 func main() {
-	fmt.Println("Hello")
-	fmt.Println("update")
+	dir, err := os.MkdirTemp("", "test-")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+	defer os.RemoveAll(dir)
+
+	fn := filepath.Join(dir, "db")
+	db, err := sql.Open("sqlite", fn)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	db.Query("select 'hello';")
 }
