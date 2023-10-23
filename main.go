ba073b3476068795626dd67df24f744503d66a6b
Aidan Farhi <aidanmcfarhi@gmail.com>
2023-10-22 22:29:01 -0400

    implemented basic add note functionality

--- main.go
+++ main.go
@@ -6,11 +6,13 @@ import (
 	"os"
 )
 
+// This represents a note.
 type Note struct {
 	Index int
 	Body  string
 }
 
+// This represents all of the notes.
 type AllNotes struct {
 	Notes []Note
 }
@@ -41,30 +43,37 @@ func getUserCommand() (string, string) {
 	return userCommand, value
 }
 
-func addNote(note, db string) {
-	// implement me
-	fmt.Println("adding notes")
-}
-
-// listNotes reads and lists notes from the specified database file.
-// It reads the JSON data from the file, unmarshals it into a struct, and prints the notes.
-// It returns an error if there is a problem reading or unmarshaling the data.
-func listNotes(db string) error {
+// getNotes loads all notes from the json file.
+func getNotes(note, db string) (*AllNotes, error) {
 	f, err := os.ReadFile(db)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return nil, err
 	}
 	var data AllNotes
 	err = json.Unmarshal(f, &data)
 	if err != nil {
 		fmt.Println(err)
-		return err
+		return nil, err
 	}
-	for i, note := range data.Notes {
-		fmt.Println(i, note.Index, note.Body)
+	return &data, nil
+}
+
+// addNote adds a new note to the json file.
+func addNote(newNote string, notes *AllNotes, db string) {
+	// TODO: remove the hardcoded 3 here and implement some sort of check index logic
+	notes.Notes = append(notes.Notes, Note{Index: 3, Body: newNote})
+	data, _ := json.MarshalIndent(notes, "", " ")
+	os.WriteFile(db, data, 0644)
+}
+
+// listNotes reads and lists notes from the specified database file.
+// It reads the JSON data from the file, unmarshals it into a struct, and prints the notes.
+// It returns an error if there is a problem reading or unmarshaling the data.
+func listNotes(notes *AllNotes) {
+	for _, note := range notes.Notes {
+		fmt.Println(note.Index, note.Body)
 	}
-	return nil
 }
 
 func main() {
@@ -82,9 +91,11 @@ func main() {
 	// handle command
 	switch userCommand {
 	case "add":
-		addNote(value, db)
+		notes, _ := getNotes(value, db)
+		addNote(value, notes, db)
 	case "ls":
-		listNotes(db)
+		notes, _ := getNotes(value, db)
+		listNotes(notes)
 	default:
 		fmt.Println("usage ./gotepad <add>|<ls>|<rm> <value>")
 	}
